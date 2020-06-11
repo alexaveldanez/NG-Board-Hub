9538491752ef556d4001f8db137a9ebbf65adee1
Alex Aveldanez <alexsaveldanez@gmail.com>
2020-06-10 23:54:08 -0700

    added navigation bars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MaterialModule } from './shared/material.module';
+import { BoardComponent } from './boards/board/board.component';
+import { BoardListComponent } from './boards/board-list/board-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    BoardComponent,
+    BoardListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
